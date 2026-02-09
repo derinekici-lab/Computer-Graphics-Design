0d0165ba6801e46deb1d54c818c645bbd95eedbc
derinekici-lab <derinekici@gmail.com>
2026-02-09 22:29:42 +0000

    Add files via upload

---
+++ CircleInSquare/SquareAndCircle.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace CGP
+{
+    public partial class SquareAndCircle : Form
+    {
+        public SquareAndCircle()
+        {
+            InitializeComponent();
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            this.Width = 500;
+            this.Height = 500;
+            this.BackColor = Color.White;
+            this.Text = "SquareAndCircle";
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            using (Pen p = new Pen(Color.Black, 2))
+            {
+                int x = 150, y = 150, size = 200;
+                g.DrawRectangle(p, x, y, size, size);
+                g.DrawEllipse(p, x, y, size, size);
+            }
+        }
+    }
+}
